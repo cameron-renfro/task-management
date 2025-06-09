e2aef2267d4644bb67e8fc1b2c861b52dfbb4850
Cameron Renfro <cameron@cameronclarkrenfro.com>
2025-06-09 18:28:29 -0400

    chore: updating headers in API to address CORS issues

--- API/src/handlers/ping.ts
+++ API/src/handlers/ping.ts
@@ -3,6 +3,11 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 export const handler: APIGatewayProxyHandler = async (event: any) => {
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+    },
     body: JSON.stringify({
       message: "pong!",
       user: event.requestContext.authorizer,
