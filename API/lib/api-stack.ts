54db3fa227d115843c47ea3e1919f0a98ef37d87
Cameron Renfro <cameron@cameronclarkrenfro.com>
2025-06-09 18:53:06 -0400

    chore: updates stack to resolve CORS issues

--- API/lib/api-stack.ts
+++ API/lib/api-stack.ts
@@ -1,34 +1,34 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 
 export class TaskApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // Lambda function
-    const pingLambda = new lambda.Function(this, "PingFunction", {
+    const pingLambda = new lambda.Function(this, 'PingFunction', {
       runtime: lambda.Runtime.NODEJS_18_X,
-      handler: "ping.handler", // file: ping.ts, function: handler
-      code: lambda.Code.fromAsset("src/handlers"), // path to folder
-    });
+      handler: 'ping.handler', // file: ping.ts, function: handler
+      code: lambda.Code.fromAsset('src/handlers'), // path to folder
+    })
 
     // REST API
-    const api = new apigateway.RestApi(this, "TaskApi", {
-      restApiName: "Task Service",
+    const api = new apigateway.RestApi(this, 'TaskApi', {
+      restApiName: 'Task Service',
       deployOptions: {
-        stageName: "dev",
+        stageName: 'dev',
       },
       defaultCorsPreflightOptions: {
-        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowOrigins: ['http://localhost:5173'], // Allow your frontend URL
         allowMethods: apigateway.Cors.ALL_METHODS,
-        allowHeaders: ["Content-Type"],
+        allowHeaders: ['Content-Type'],
       },
-    });
+    })
 
     // /ping endpoint
-    const ping = api.root.addResource("ping");
-    ping.addMethod("GET", new apigateway.LambdaIntegration(pingLambda));
+    const ping = api.root.addResource('ping')
+    ping.addMethod('GET', new apigateway.LambdaIntegration(pingLambda))
   }
 }
