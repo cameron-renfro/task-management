a7f66ac80f6eae0a087e17bd2dbe0dc72bb36d48
Cameron Renfro <cameron@cameronclarkrenfro.com>
2025-06-09 18:26:50 -0400

    chore: implementing frontend fetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const url =
+      "https://zn6wvmciu6.execute-api.us-east-1.amazonaws.com/dev/ping";
+
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data fetched:", data);
+        setMessage(data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setMessage("Failed to fetch data");
+      });
+  }, []);
 
   return (
     <>
@@ -28,8 +50,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      {message && (
+        <div>
+          <p>{message}</p>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
